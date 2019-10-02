bfbb19f608b6131f84c1c1280cf3e9949735eeda
Walicen <walicen.r@gmail.com>
2019-10-02 18:54:44 -0300

    create frontend web

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,16 +1,21 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
+const path = require('path');
+
 const routes = require('./routes');
 
 const app = express();
 
-let url = 'SUA_URL_CONEXAO_MONGO';
+const url = 'SUA_URL_CONEXAO_MONGO';
 mongoose.connect(url, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
 
+app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
 app.listen(3333);
